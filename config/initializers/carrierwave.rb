de8f9a03792e4a57c7900877cdbf891ebce9620e
Dane Wilson <dnkw@hotmail.com>
2014-05-09 12:25:06 -0400

    Added Cloudfront support

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,6 @@ CarrierWave.configure do |config|
 		config.fog_directory  = 'damagecontrolfamily'                     # required
 		config.fog_public     = false                                   # optional, defaults to true
 		config.storage = :fog
-		# config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 	else
 		config.storage = :file
 	end
