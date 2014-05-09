8bb6558fa4b0d3c004c3754db049a63e652577d3
Dane Wilson <dnkw@hotmail.com>
2014-05-09 13:48:46 -0400

    Made Changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,14 @@
 CarrierWave.configure do |config|
 
 	config.fog_credentials = {
-		:provider               => 'AWS',                        # required
-		:aws_access_key_id      => '<%= ENV['AWS_ACCESS_KEY_ID'] %>',                        # required
-		:aws_secret_access_key  => '<%= ENV['AWS_SECRET_ACCESS_KEY'] %>',                        # required
+		:provider               => "AWS",                        # required
+		:aws_access_key_id      => "<%= ENV['AWS_ACCESS_KEY_ID'] %>",                        # required
+		:aws_secret_access_key  => "<%= ENV['AWS_SECRET_ACCESS_KEY'] %>",                        # required
 		# :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
 		# :host                   => 's3.example.com',             # optional, defaults to nil
 		# :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
 	}
-	config.fog_directory  = 'damagecontrolfamily'                  		# required
+	config.fog_directory  = "damagecontrolfamily"                  		# required
 	config.fog_public     = true                                   		# optional, defaults to true
 	config.asset_host	  = "http://assets%d.damagecontrolfamily.com"  	# optional, defaults to true
   	config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  	# optional, defaults to {}
@@ -21,5 +21,3 @@ CarrierWave.configure do |config|
 		config.storage = :fog
 	end
 end
-
-heroku config:add AWS_ACCESS_KEY_ID=
