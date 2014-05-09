aeb609c329cf407d99b9018b087915d5bbd49966
Dane Wilson <dnkw@hotmail.com>
2014-05-09 15:40:46 -0400

    Editted config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,13 +8,13 @@ CarrierWave.configure do |config|
 		config.storage = :fog
 		config.fog_credentials = {
 			:provider               => "AWS",                        # required
-			:aws_access_key_id      => Damagemiseh::Application.config.aws_key,                        # required
-			:aws_secret_access_key  => Damagemiseh::Application.config.aws_secret,                        # required
+			:aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+			:aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                        # required
 			# :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
 			# :host                   => 's3.example.com',             # optional, defaults to nil
 			# :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
 		}
-		config.fog_directory  = "damagecontrolfamily"                  		# required
+		config.fog_directory  = ENV['FOG_DIRECTORY']                 		# required
 		config.fog_public     = true                                   		# optional, defaults to true
 		config.asset_host	  = "http://assets%d.damagecontrolfamily.com"  	# optional, defaults to true
 	  	config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  	# optional, defaults to {}
